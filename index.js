3b48c5ed0f515934fe7ca3418d019ceb68d7bd7f
Khairul Anuar <65583587+ckhairulanuar@users.noreply.github.com>
2021-07-03 14:51:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hai semua! Azure App Services, Hosted on Github. Runtime Nodejs 12 on Linux");
 });
 
 const port = process.env.PORT || 1337;
